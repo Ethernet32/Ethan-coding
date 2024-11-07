150d3218880ba71f0585a0f8e249e055e7e43120
Ethernet32 <ethan.ralphs@ucas-edu.net>
2024-11-06 21:04:02 -0700

    finished practice
    finished

--- tech.py
+++ tech.py
@@ -1,8 +1,32 @@
 class Tech:
-    pass
+    def __init__(self, name, storage):
+        self.name = name
+        self.storage = storage
 
 class Phone(Tech):
-    pass
+    def __init__(self, name, storage, color):
+        super().__init__(name, storage)
+        self.color = color
+    
+    def __str__(self):
+        return f"A {self.color} {self.name} with {self.storage}GB of storage"
+
+    def __repr__(self):
+        return f"Phone({self.name}, {self.storage}, {self.color})"
 
 class Laptop(Tech):
-    pass
+    def __init__(self, name, storage, size):
+        super().__init__(name, storage)
+        self.size = size
+    
+    def __str__(self):
+        return f"{self.size}-inch {self.name} with {self.storage}GB of storage."
+    
+    def __repr__(self):
+        return f"Laptop({self.name}, {self.storage}, {self.size})"
+
+phone = Phone("Galaxy", 126, "Gray")
+laptop = Laptop("chrome book", 45, 45)
+
+print(repr(phone))
+print(repr(laptop))
