b577420f1e4e6cdc058065cef2650b013e3fcc6b
ethernet32 <ethan.ralphs@ucas-edu.net>
2024-11-20 11:24:36 -0700

    made tech folder and fixed import tech

---
+++ Tech_Folder/main.py
@@ -0,0 +1,9 @@
+from tech import *
+
+
+
+phone = Phone("Galaxy", 126, "Gray")
+laptop = Laptop("chrome book", 45, 45)
+
+print(repr(phone))
+print(repr(laptop))
--- Tech_Folder/tech.py
+++ Tech_Folder/tech.py
@@ -25,8 +25,3 @@ class Laptop(Tech):
     def __repr__(self):
         return f"Laptop({self.name}, {self.storage}, {self.size})"
 
-phone = Phone("Galaxy", 126, "Gray")
-laptop = Laptop("chrome book", 45, 45)
-
-print(repr(phone))
-print(repr(laptop))
